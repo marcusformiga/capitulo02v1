424687d26c16d39ee94a3abe703193561a6b78d8
marcusformiga <kiko.antares@gmail.com>
2021-09-10 09:28:47 -0300

    feature: criação do provider para trabalhar com datas dentro da nossa aplicação

--- src/shared/providers/Date/DayJs.ts
+++ src/shared/providers/Date/DayJs.ts
@@ -16,4 +16,9 @@ export class DayjsProvider implements IDateProvider {
     const start_date_utc = this.convertToUtc(start_date);
     return dayjs(end_date).diff(start_date, "hours");
   }
+  compareInDays(start_date: Date, end_date: Date): number {
+    const end_date_utc = this.convertToUtc(end_date);
+    const start_date_utc = this.convertToUtc(start_date);
+    return dayjs(end_date).diff(start_date, "days");
+  }
 }
--- src/shared/providers/Date/IDateProvider.ts
+++ src/shared/providers/Date/IDateProvider.ts
@@ -2,4 +2,5 @@ export interface IDateProvider {
   compareDateInHours(start_date: Date, end_date: Date): number;
   convertToUtc(date: Date): string;
   dateNow(): Date;
+  compareInDays(start_date: Date, end_date: Date): number;
 }
