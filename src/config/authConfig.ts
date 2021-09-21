1092a5c5ed6b7190484e1d1737ed0f72cfc216c4
marcusformiga <kiko.antares@gmail.com>
2021-09-21 12:40:57 -0300

    feature: arquivo de configuraçoes jwt

---
+++ src/config/authConfig.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "husaehasuehaushe",
+  token_expires_in: "15m",
+  secret_refresh_token: "ashesahgdesaeas",
+  secret_tokenrefresh_expires_in: "30d",
+  expires_refreshtoken_in_days: 30,
+};
